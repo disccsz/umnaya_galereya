db60557755549401427ce58ad063cd2fc5265ab0
Артём Т <my@disccsz.ru>
2026-07-14 23:05:20 +0700

    Создан FastAPI структура проекта и config

---
+++ app/core/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    # --- PostgreSQL ---
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    # --- MinIO ---
+    MINIO_ENDPOINT: str
+    MINIO_ROOT_USER: str
+    MINIO_ROOT_PASSWORD: str
+    MINIO_BUCKET_NAME: str
+
+    # Магия: указываем, откуда брать переменные
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+    @property
+    def database_url(self) -> str:
+        """Автоматически собирает строку подключения для SQLAlchemy"""
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+# Создаем единственный экземпляр класса. 
+# При первом импорте этого файла Pydantic сам прочитает .env и заполнит поля.
+settings = Settings()
